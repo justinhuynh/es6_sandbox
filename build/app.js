a5566ac9dc3f0cb611da1d4e9c97d59db3ed0fa9
justinhuynh <jhuynh@gmail.com>
2016-06-24 12:48:07 -0400

    Added sandbox

---
+++ build/app.js
@@ -0,0 +1,1 @@
+// If you want to sandbox things to run in your Chrome console, do it here
---
+++ src/main.js
@@ -0,0 +1,2 @@
+// Stuff you write here will be executed when the page is loaded
+// But you won't be able to access it from the Chrome console
